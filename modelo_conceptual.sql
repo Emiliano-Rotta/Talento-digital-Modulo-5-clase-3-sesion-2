1894cd6fe4358b757c12175cceab6b0b99c35513
Emiliano Rotta <emilianorottaf@gmail.com>
2024-10-09 19:57:15 -0300

    first commits

---
+++ modelo_conceptual.sql
@@ -0,0 +1,46 @@
+-- CADA TABLA TIENE QUE TENER 4 datos minimo, colocar la Primary key y la foreign key
+
+-- Ejercicio 1: Sistema de Hospital 
+-- Consigna
+-- Un hospital quiere organizar los datos de los pacientes, doctores, y citas. Crea las tablas correspondientes y establece las siguientes relaciones:
+
+-- Cada paciente tiene un único doctor de cabecera, y cada doctor puede tener múltiples pacientes asignados.
+
+-- Un paciente puede tener múltiples citas, pero una cita solo pertenece a un paciente.
+
+-- Cada cita puede involucrar a varios doctores, y un doctor puede estar involucrado en varias citas.
+
+-- Tablas a crear: pacientes, doctores, citas, y una tabla intermedia para la relación entre doctores y citas.
+
+
+
+
+
+-- Ejercicio 2: Sistema de Universidad
+-- Consigna
+
+-- Una universidad quiere registrar los datos de los estudiantes, cursos, y profesores. Crea las tablas correspondientes y establece las siguientes relaciones:
+
+-- Un profesor puede enseñar varios cursos, pero un curso solo puede tener un profesor.
+
+-- Un estudiante puede estar inscrito en varios cursos, y un curso puede tener varios estudiantes inscritos.
+
+-- Cada estudiante tiene un único tutor académico, y cada tutor puede estar asignado a varios estudiantes.
+
+-- Tablas a crear: estudiantes, profesores, cursos, y una tabla intermedia para la relación entre estudiantes y cursos.
+
+
+
+
+
+
+-- Ejercicio 3: Sistema de Biblioteca
+-- Consigna
+-- Una biblioteca quiere organizar los datos de los libros, categorías, autores y préstamos. Crea las tablas correspondientes y establece las siguientes relaciones:
+
+-- tener en cuenta la relacion entre libros y categorias (cada libro solo puede tener una categoria)
+
+-- libros autores (contemplar que hay libros escritos por varios autores)
+
+-- prestamos de libros y usuarios (Un usuario puede pedir prestados varios libros, pero un préstamo pertenece solo a un libro y a un usuario.
+-- )
